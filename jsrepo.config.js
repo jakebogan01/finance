5e858aed6f2589af761cda1b8da92a5cdb263630
Jake Bogan <jake@Jakes-MacBook-Air.local>
2026-03-05 10:30:39 -0500

    installed shadcn-svelte-extras

---
+++ jsrepo.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'jsrepo';
+
+export default defineConfig({
+    // configure where stuff comes from here
+    registries: ['@ieedan/shadcn-svelte-extras'],
+    // configure where stuff goes here
+    paths: {
+		ui: 'src/lib/components/ui',
+		block: 'src/lib/components',
+		hook: 'src/lib/hooks',
+		action: 'src/lib/actions',
+		util: 'src/lib/utils',
+		lib: 'src/lib'
+	},
+});
