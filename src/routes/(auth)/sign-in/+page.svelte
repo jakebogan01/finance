0010cf62b96cb71b5600b95580fe7bc4145a8748
Jake Bogan <jake@Jakes-MacBook-Air.local>
2026-03-05 17:29:06 -0500

    sign-in - added form validation

--- src/routes/(auth)/sign-in/+page.svelte
+++ src/routes/(auth)/sign-in/+page.svelte
@@ -1,16 +1,38 @@
+<script module>
+	import * as zod from 'zod';
+
+	const schema = zod.object({
+		email: zod
+			.string({ required_error: 'Email address is required' })
+			.trim()
+			.toLowerCase()
+			.min(1, { message: 'Email is required' })
+			.max(254, { message: 'Email must be less than 255 characters' })
+			.email({ message: 'Please enter a valid email address' }),
+		password: zod
+			.string({ required_error: 'Password is required' })
+			.min(8, { message: 'Password must be at least 8 characters long' })
+			.max(71, { message: 'Password must be less than 71 characters' })
+	});
+</script>
+
 <script>
 	import FormButton from '$lib/components/FormButton.svelte';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import * as Password from '$lib/components/ui/password';
+	import { validator } from '@felte/validator-zod';
+	import reporterDom from '@felte/reporter-dom';
 	import { resolve } from '$app/paths';
 	import pb from '$lib/pocketbase.js';
 	import { createForm } from 'felte';
+	import FieldErrors from '$lib/components/FieldErrors.svelte';
 
 	const { form, reset } = createForm({
 		initialValues: {
 			email: null,
 			password: null
 		},
+		extend: [validator({ schema }), reporterDom()],
 		onSubmit: async (values) => {
 			try {
 				await pb.collection('users').authWithPassword(values.email, values.password);
@@ -35,9 +57,11 @@
 			maxlength={255}
 			required
 		/>
+		<FieldErrors ref="email" />
 	</div>
 
 	<div>
+		<label for="password" class="sr-only">Password</label>
 		<Password.Root>
 			<Password.Input
 				id="password"
@@ -51,6 +75,7 @@
 				<Password.ToggleVisibility />
 			</Password.Input>
 		</Password.Root>
+		<FieldErrors ref="password" />
 	</div>
 
 	<div class="space-y-5">
@@ -60,8 +85,9 @@
 
 		<p class="text-preset-2 text-center text-grey-50">
 			Don't have an account?
-			<a href={resolve('/sign-up')} class="md:transition-colors md:hover:text-yellow-200">Sign up</a
-			>
+			<a href={resolve('/sign-up')} class="md:transition-colors md:hover:text-yellow-200">
+				Sign up
+			</a>
 		</p>
 	</div>
 </form>
