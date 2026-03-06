ed1ada1ebe7444b1c8635820181ac492ee8adf94
Jake Bogan <jake@Jakes-MacBook-Air.local>
2026-03-06 09:19:22 -0500

    app - started designing sidebar component

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { DASHBOARD, INCOME, EXPENSES } from '$lib/utils/constants';
+	import LayoutGridIcon from '@lucide/svelte/icons/layout-grid';
+	import HandCoinsIcon from '@lucide/svelte/icons/hand-coins';
+	import { INCOMESLUG } from '$lib/stores/incomeSlug.svelte';
+	import ReceiptIcon from '@lucide/svelte/icons/receipt';
+	import logo from '$lib/assets/graphics/logo.svg';
+	import { resolve } from '$app/paths';
+
+	let { mobile = false } = $props();
+
+	const navItems = $derived([
+		{
+			page: 'Dashboard',
+			icon: LayoutGridIcon,
+			link: DASHBOARD
+		},
+		{
+			page: 'Income',
+			icon: HandCoinsIcon,
+			link: INCOME + `${INCOMESLUG.value ?? ''}`
+		},
+		{
+			page: 'Expenses',
+			icon: ReceiptIcon,
+			link: EXPENSES
+		}
+	]);
+</script>
+
+<div
+	class={[
+		'flex grow flex-col gap-y-13 overflow-y-auto border-grey-300 bg-grey-1000 px-5 py-8',
+		mobile && 'relative border-r',
+		!mobile && 'rounded-30 border'
+	]}
+>
+	<div class={['flex shrink-0 items-center justify-center space-x-2', mobile && 'relative']}>
+		<img src={logo} alt="Finance logo" class="h-8 w-auto" />
+		<span class="text-preset-7-bold text-white-0">Finance</span>
+	</div>
+	<nav aria-label="Primary navigation" class={['flex flex-1 flex-col', mobile && 'relative']}>
+		<ul role="list" class="flex flex-1 flex-col gap-y-13">
+			<li>
+				<ul role="list" class="space-y-1.5">
+					{#each navItems as { page, icon: Icon, link }, i (i)}
+						<li>
+							<a
+								href={resolve(link)}
+								class="text-preset-4-semibold flex items-center gap-x-4.5 rounded-lg bg-transparent p-4 text-grey-50 data-[active=true]:bg-yellow-100 data-[active=true]:text-grey-600 md:transition-colors md:data-[active=false]:hover:text-white-0"
+							>
+								<Icon class="size-5 shrink-0" strokeWidth="1.5" />
+								{page}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</div>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
+	import Sidebar from '$lib/components/Sidebar.svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="relative mx-auto flex w-full max-w-367.5 flex-1 flex-col px-4 py-5 sm:px-5">
+	<div class="hidden 1126:fixed 1126:inset-y-5 1126:z-50 1126:flex 1126:w-60 1126:flex-col">
+		<Sidebar />
+	</div>
+
+	<div class="flex flex-1 flex-col space-y-5 1126:pl-65">
+		<main class="relative grid gap-5 lg:flex-1 lg:grid-cols-[9fr_8fr]">
+			{@render children?.()}
+		</main>
+	</div>
+</div>
