a65590767c47b8a9d0b20601e439ba3c920b5829
jake <jakebogan01@gmail.com>
2026-03-21 14:51:24 -0400

    updated Head component

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { page } from '$app/state';
 
-	let { page_title = null, description } = $props();
+	let { page_title = 'Finance', description = '' } = $props();
 </script>
 
 <svelte:head>
 	<title>{page_title}</title>
 	<meta name="description" content={description} />
-	<meta property="og:url" content={page.url} />
+	<meta property="og:url" content={page.url.href} />
 	<meta property="og:description" content={description} />
+	<link rel="canonical" href={page.url.href} />
 </svelte:head>
