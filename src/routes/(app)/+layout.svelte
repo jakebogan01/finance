527585af776489011f97df33e8c351b90900a5e6
jake <jakebogan01@gmail.com>
2026-04-01 12:39:17 -0400

    updated body styles

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -16,29 +16,31 @@
 
 <MobileSidebar />
 
-<div
-	class={[
-		'relative mx-auto flex w-full max-w-400 flex-1 flex-col px-4 py-5 sm:px-5 3xl-tall:ultra-screen',
-		darkMode.status ? 'dark' : ''
-	]}
->
-	<div class="hidden xl:fixed xl:inset-y-5 xl:z-50 xl:flex xl:w-66 xl:flex-col 3xl-tall:absolute">
-		<Sidebar />
-	</div>
+<div class="flex flex-1 flex-col 3xl-tall:items-center 3xl-tall:justify-center 3xl-tall:p-10">
+	<div
+		class={[
+			'relative mx-auto flex w-full max-w-400 flex-1 flex-col px-4 py-5 sm:px-5 3xl-tall:ultra-screen',
+			darkMode.status ? 'dark' : ''
+		]}
+	>
+		<div class="hidden xl:fixed xl:inset-y-5 xl:z-50 xl:flex xl:w-66 xl:flex-col 3xl-tall:absolute">
+			<Sidebar />
+		</div>
 
-	<div class="flex flex-1 flex-col space-y-5 xl:pl-71">
-		<Topbar />
+		<div class="flex flex-1 flex-col space-y-5 xl:pl-71">
+			<Topbar />
 
-		{#if page.url.pathname === EXPENSES || page.url.pathname === INCOME}
-			<main
-				class="relative grid gap-5 overflow-hidden rounded-2xl border border-grey-300 bg-grey-1000 p-4 sm:rounded-30 sm:p-5 lg:flex-1 lg:grid-cols-[10fr_8fr] dark:bg-white-0"
-			>
-				{@render children?.()}
-			</main>
-		{:else}
-			<main class="relative grid gap-5 lg:flex-1 lg:grid-cols-[10fr_8fr]">
-				{@render children?.()}
-			</main>
-		{/if}
+			{#if page.url.pathname === EXPENSES || page.url.pathname === INCOME}
+				<main
+					class="relative grid gap-5 overflow-hidden rounded-2xl border border-grey-300 bg-grey-1000 p-4 sm:rounded-30 sm:p-5 lg:flex-1 lg:grid-cols-[10fr_8fr] dark:bg-white-0"
+				>
+					{@render children?.()}
+				</main>
+			{:else}
+				<main class="relative grid gap-5 lg:flex-1 lg:grid-cols-[10fr_8fr]">
+					{@render children?.()}
+				</main>
+			{/if}
+		</div>
 	</div>
 </div>
